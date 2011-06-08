4b9f6e14e6c44b6d1da4f33082d36ffc019186f8
root <root@buda.tiger.railwaymen.org>
2011-06-08 17:42:25 +0200

    Yaml config

--- lib/cloud_monitor.rb
+++ lib/cloud_monitor.rb
@@ -1,13 +1,14 @@
 require 'active_support/notifications'
 require 'cloud_monitor/middleware'
 require 'cloud_monitor/engine'
+require "yaml"
 
 ActiveSupport::Notifications.subscribe do |*args|
   CloudMonitor.store!(args) unless CloudMonitor.mute?
 end
 
 module CloudMonitor
-  Config = Yaml.load(File.read("#{Rails.root}/config/cloud_monitor.yml")).symbolize_keys
+  #Config = YAML.load(File.read("#{Rails.root}/config/cloud_monitor.yml")).symbolize_keys
   @store = []
   def self.path
     File.expand_path('../..', __FILE__)
@@ -15,7 +16,8 @@ module CloudMonitor
 
   def self.store!(*args)
     @store << args
-    
+    file_name = File.join([Rails.root, 'log', 'monitor', (Time.new.to_f * 100000).to_i.to_s(16).upcase+".cm"])
+    File.open(file_name, 'w') {|f| f.write(args.to_yaml) }
   end
 end
 
